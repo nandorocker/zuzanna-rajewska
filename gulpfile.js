6c83235d31c8cafe038bad7fa789b3b4d94418af
Nando Rossi <talk@nan.do>
2016-03-07 01:24:49 +0100

    Adds code comments, environent variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util')
 	jade = require('jade'),
-	source = require('vinyl-source-stream'),
-	buffer = require('vinyl-buffer'),
 	gulpJade = require('gulp-jade'),
-	browserify = require('browserify'),
+	sass = require('gulp-sass'),
+	browserify = require('browserify'), // Does some magic shit I don't understand
+	source = require('vinyl-source-stream'), // This and the next one required for browserify
+	buffer = require('vinyl-buffer'),
 	uglify = require('gulp-uglify');
 
-var env = process.env.NODE_ENV || 'development';
+// Sets environment variables through gulp-util
+// To invoke: $ gulp --env=prod
+var env = gutil.env.env;
+var outputDir = '.tmp';
 
-gulp.task('jade', function(){
+if (env === 'prod') {
+	outputDir = 'dist';
+	console.log("hello");
+}
+if (env === 'dev') {
+}
+
+
+// Process HTML
+gulp.task('html', function(){
 	return gulp.src('app/**/*.jade')
 		.pipe(gulpJade({
 			jade: jade,
 			pretty: true
 		}))
-		.pipe(gulp.dest('.tmp/'))
+		.pipe(gulp.dest('outputDir'))
 });
 
+// Process scripts
 gulp.task('js', function() {
 	return browserify('./app/scripts/main.js', { debug: env !== 'production' })
 		.bundle()
@@ -27,6 +41,23 @@ gulp.task('js', function() {
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest('.tmp/scripts'))
 		.pipe(buffer())
-		.pipe(gutil.env.env === 'prod' ? uglify() : gutil.noop())
-		.pipe(gulp.dest('.tmp/scripts'))
+		.pipe(env === 'prod' ? uglify() : gutil.noop())
+		.pipe(gulp.dest(outputDir + '/scripts'))
+});
+
+// Process styles
+gulp.task('styles', function(){
+	var config = {};
+
+	if (env === 'prod') {
+		config.outputStyle = 'compressed';
+	}
+
+	if (env === 'dev') {
+		config.outputStyle = 'map';
+	}
+
+	return gulp.src('app/styles/**/*.{scss,sass}')
+		.pipe(sass(config))
+		.pipe(gulp.dest(outputDir + '/styles'))
 });
